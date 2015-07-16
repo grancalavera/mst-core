a38fb2a3b004090c4c5719d0f006da8eeea38be3
Leon Coto <leoncoto@gmail.com>
2015-07-16 07:51:38 +0100

    Adding nunjucks and a filter

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ var Metalsmith = require('metalsmith')
   , layouts = require('metalsmith-layouts')
   , inplace = require('metalsmith-in-place')
   , permalinks = require('metalsmith-permalinks')
+  , nunjucks = require('nunjucks')
+
+
+nunjucks
+  .configure({watch: false})
+  .addFilter('stretch', function(str) {
+    return str.split('').map(function(char) {
+      var stretched = '', i = 0
+      for (i; i < 5; i++) { stretched += char }
+      return stretched
+    }).join('').toLowerCase()
+  })
 
 Metalsmith(__dirname)
   .source('_posts')
   .use(markdown())
   .use(permalinks({pattern: 'blog/:author/:date/:title'}))
-  .use(layouts({engine: 'handlebars', directory: '_layouts'}))
-  .use(inplace({engine: 'handlebars'}))
+  .use(layouts({engine: 'nunjucks', directory: '_layouts'}))
+  .use(inplace({engine: 'nunjucks'}))
   .build(function errorHandler(err) {
     if (!err) return
     console.error(err.message)
     process.exit(1)
   })
-
 
